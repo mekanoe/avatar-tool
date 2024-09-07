cf4e1803570f15166d226614bcf3b02f94c2434b
noe <git@kat.cafe>
2024-09-06 21:19:54 -0700

    weighted random

--- functions/[group].js
+++ functions/[group].js
@@ -10,12 +10,35 @@ export async function onRequest(context) {
   const groupData = groups[groupName];
   const weights = groupData.weights;
 
-  // TODO: weighted random
-  const files = Object.keys(weights);
-  const pickedFile = files[Math.floor(Math.random() * files.length)];
+  const pickedFile = weightedRandomKey(weights);
+  console.log({ pickedFile });
 
   const fullPath = `/${groupName}/${pickedFile}`;
   const url = new URL(context.request.url);
   url.pathname = fullPath;
   return context.env.ASSETS.fetch(url);
 }
+
+const weightedRandom = (choices, weights) => {
+  const n = choices.length;
+
+  const opts = choices.map((x, i) => [weights[i], x]);
+  const total = opts.reduce((acc, o) => acc + o[0], 0);
+
+  const r = Math.floor(Math.random() * total);
+  let sum = total;
+  for (let i = 0; i < n; i++) {
+    sum -= opts[i][0];
+    if (sum <= r) {
+      return opts[i][1];
+    }
+  }
+};
+
+const weightedRandomKey = (choices) => {
+  const keys = Object.keys(choices);
+  return weightedRandom(
+    keys,
+    keys.map((x) => choices[x])
+  );
+};
